81b91a104fcd4b96b0a62cbaf8728a36e021e0f7
Jesus Medina <chuymedina96@gmail.com>
2017-07-06 22:11:04 +0000

    added new route for expressExercise

--- ExpressScratch/expressExercise/app.js
+++ ExpressScratch/expressExercise/app.js
@@ -16,12 +16,13 @@ app.get("/sound/:animal", function(req, res){
     var sound = sounds[animal];
     res.send("The " + animal + " goes " + sound);
 });
+app.get("/repeat/:message/:times", function(req, res){
+    var message = req.params.message.toUpperCase();
+    var times = req.params.times;
+    res.send("Message: " + message + "Times: " + times);
+});
 app.get("*", function(req, res){
     res.send("These are not the drones you are looking for.");
-});
-app.get("/repeat/hello/:number", function (req, res){
-    var number = req.params.number;
-    res.send("hello" * number);
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
