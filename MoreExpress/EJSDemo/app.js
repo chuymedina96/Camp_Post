4e079f1982540815168ad13c745fe335357657bb
Jesus Medina <chuymedina96@gmail.com>
2017-07-11 18:16:16 +0000

    loops, arrays, and objects in express

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -4,12 +4,22 @@ var app = express();
 app.get("/", function(req, res){
     res.render("index.ejs");
 });
-app.get("/fell/:thing/:chuy", function(req, res){
+app.get("/fell/:thing/:chuy/:random", function(req, res){
     var thing = req.params.thing;
     var chuy = req.params.chuy;
-    res.render("love.ejs", {thingVar: thing, me:chuy});
+    var random = req.params.random;
+    res.render("love.ejs", {thingVar: thing, me:chuy, Random: random});
+});
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Hangingin out enjoying this day", author: "me"},
+        {title: "chillin", author: "chris"},
+        {title: "coolin", author: "chuy"},
+        {title: "aweomse", author: "lali"}
+    ];
+    res.render("posts.ejs", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The server has started bitch!!!");
+    console.log("The server has started!!");
 })
