8b3c6fa28c71135aebbfa69824f7b8aa7aacd15f
Jesus Medina <chuymedina96@gmail.com>
2017-07-11 20:45:26 +0000

    more express examples

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("index.ejs");
+    res.render("index");
 });
 app.get("/fell/:thing/:chuy/:random", function(req, res){
     var thing = req.params.thing;
     var chuy = req.params.chuy;
     var random = req.params.random;
-    res.render("love.ejs", {thingVar: thing, me:chuy, Random: random});
+    res.render("love", {thingVar: thing, me:chuy, Random: random});
 });
 app.get("/posts", function(req, res){
     var posts = [
@@ -17,7 +20,7 @@ app.get("/posts", function(req, res){
         {title: "coolin", author: "chuy"},
         {title: "aweomse", author: "lali"}
     ];
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
